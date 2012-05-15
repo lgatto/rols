e1f263dbb1bb280af4b424a10c3f619632ccd203
Marc Carlson <mcarlson@fhcrc.org>
2012-05-15 19:17:10 +0000

    Adds rols to the repos.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rols@65922 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/AllGenerics.R
@@ -0,0 +1,4 @@
+setGeneric("key", function(object) standardGeneric("key"))
+setGeneric("value", function(object) standardGeneric("value"))
+setGeneric("map", function(object) standardGeneric("map"))
+
